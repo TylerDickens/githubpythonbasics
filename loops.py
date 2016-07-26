0a6c574fd61a45d8bf3e6fa88a5a9d5411815896
Tyler Dickens <tytydickens@gmail.com>
2016-07-26 18:41:57 +0000

    first commit

---
+++ loops.py
@@ -0,0 +1,25 @@
+#loops in python
+
+abc = [24,45,74,65,95,]
+dez = [658,947,533,748,]
+ef = ["jelly", "jar", "cars"]
+
+for x in abc:
+    print x
+    
+for x in ef:
+    if x == "michael":
+        print"yeah michael"
+    else:
+            print"nothing"
+            
+for x in dez:
+    if x == 44:
+        print "something"
+    elif x == 566
+        print"you made it"
+        
+for x in range(-5,6):
+        print x 
+        
+
